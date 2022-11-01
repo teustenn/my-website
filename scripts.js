92898500f60984a096c9b67ae62e7a44d828292f
teustenn <teustenn@gmail.com>
2022-11-01 17:53:29 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+/* Scroll */
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("scroll");
+var sticky = header.offsetTop;
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
